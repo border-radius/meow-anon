0ec63e644873c4b40b36a13ae6f111e562d56813
border-radius <pharshmaster@gmail.com>
2015-07-27 08:26:46 +0000

    Header

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ var cfg = require('./server.json');
 
 var app = express();
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public/'));
 
